9acc120ceb8eac21ca4e23bf871927284498c347
yerraabhi <yerraabhiram131@gmail.com>
2025-12-03 17:56:42 +0530

    initial commit

---
+++ python codingal/operators.py
@@ -0,0 +1,13 @@
+first_name="Codingal"
+last_name="Educations"
+full_name=first_name+last_name
+example="Haa"*5
+print(first_name)
+print(last_name)
+print(full_name)
+print("String Multiplied Five Times Gives This Result:", example)
+word="coding"
+print("Length Of String:", len(word))
+print("First Letter Of String", word[0])
+print("Last Letter Of String", word[5])
+print("String Sliced:", word[0:3])
---
+++ python codingal/sentences.py
@@ -0,0 +1,7 @@
+fn="Abhiram"
+ln="Yerra"
+a="10"
+g=" grade 4"
+print("My name is ", fn, ln)
+print("My age is ", a)
+print("I study in ", g)
