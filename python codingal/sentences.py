365900213321659278294d05b513006456d89a45
yerraabhi <yerraabhiram131@gmail.com>
2025-12-04 16:25:51 +0530

    initial commit

--- python codingal/sentences.py
+++ python codingal/sentences.py
@@ -2,6 +2,6 @@ fn="Abhiram"
 ln="Yerra"
 a="10"
 g=" grade 4"
-print("My name is ", fn, ln)
-print("My age is ", a)
-print("I study in ", g)
+print("My name is", fn, ln)
+print("My age is", a)
+print("I study in", g)
