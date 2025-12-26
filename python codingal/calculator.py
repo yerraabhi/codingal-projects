62ae918f344c2b6f88bdba894beb2639b0a586fb
yerraabhi <yerraabhiram131@gmail.com>
2025-12-26 13:57:48 +0530

    initial commit

---
+++ python codingal/calculator.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter a number1 for addition:"))
+n2=int(input("Enter a number2 for addition:"))
+sum = n1+n2
+def addition(n1,n2,sum):
+    print(n1,"+",n2,"=",sum)
+
+n3=int(input("Enter a number1 for multiplication:"))
+n4=int(input("Enter a number2 for multiplication:"))
+prod = n3 * n4
+def multiplication(n3,n4,prod):
+    print(n3,"x",n4,"=",prod)
