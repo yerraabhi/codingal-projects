df9c9d52423e2d0f151d86a12e2ccd91947ce262
yerraabhi <yerraabhiram131@gmail.com>
2026-01-30 20:48:49 +0530

    initial commit

---
+++ python codingal/age-check.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+
+if age>10:
+    if age<20:
+        print("Your age is beetween 10-20 years old.")
+    else:
+        print("Your age is not beetween 10-20 years old")
+else:
+    print("Your age is not beetween 10-20 years old")
---
+++ python codingal/whileloops.py
@@ -0,0 +1,14 @@
+# naturalnumber=int(input("Enter a natural number:"))
+# i=1
+# sum=0
+# while i<=naturalnumber:
+#     sum=sum+i
+#     i+=1
+
+# print("Sum=",sum)
+
+#Write a program to check the infinite loop?
+
+# i=1
+# while i>0:
+#     print(i)
