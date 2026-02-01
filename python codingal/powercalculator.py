12e3a53e01fc3137289a0f632d4e4dca5cb3a8e0
yerraabhi <yerraabhiram131@gmail.com>
2026-02-01 10:16:06 +0530

    initial commit

---
+++ python codingal/powercalculator.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter a number for the power calculation:"))
+num2=int(input("Enter a number for the nummber of power:"))
+
+poweranswer=num1**num2
+
+print(num1,"^",num2,"=",poweranswer)
