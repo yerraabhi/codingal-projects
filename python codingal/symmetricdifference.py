ed1489a0ae52c7a3b0fb751ce39815a16da9a3c4
yerraabhi <yerraabhiram131@gmail.com>
2026-03-29 16:23:07 +0530

    initial commit

---
+++ python codingal/symmetricdifference.py
@@ -0,0 +1,6 @@
+set1={"blue","green"}
+set2={"blue","yellow"}
+set3={1,2,3,4,5}
+set4={1,5,6,7,8,9}
+print("Symmetric difference1=",set1.symmetric_difference(set2))
+print("Symmetric difference2=",set3.symmetric_difference(set4))
