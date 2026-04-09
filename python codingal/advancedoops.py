08828e16a8bde2cbae3986be82bb09e3db951656
yerraabhi <yerraabhiram131@gmail.com>
2026-04-09 18:52:19 +0530

    initial commit

--- python codingal/advancedoops.py
+++ python codingal/advancedoops.py
@@ -31,3 +31,53 @@ parrot1=parrot("Parro",7)
 parrot2=parrot("Parot",5)
 print(parrot1.name,"is a",parrot1.species,"and age=",parrot1.age)
 print(parrot2.name,"is a",parrot2.species,"and age=",parrot2.age)
+
+#Create a class IOString which consists of a constructor that gives a default value to variable str1. Next up create a method that gets a string as input from the user. Create another method that will print the string in the upper case. Next up create an object and call methods to get everything implemented.
+
+class IOString:
+    def __init__(self):
+        self.str1=""
+    
+    def getstring(self):
+        self.str1=input("Enter String:")
+    
+    def printstring(self):
+        print("Result is:",self.str1.upper())
+
+str1=IOString()
+
+str1.getstring()
+str1.printstring()
+
+#Create a class with the named employee and create a constructor and destructor. Then, write a function to create an object for that class and delete the object. Make sure you call the function to get everything implemented!
+
+class Employee:
+    def __init__(self):
+        print("Employee created")
+    
+    def __del__(self):
+        print("Destructor called")
+
+def createobj():
+    print("Making Object")
+    obj=Employee
+    print("Function End")
+    return obj
+
+print("Calling createobj() function")
+obj=createobj()
+print("Program End")
+
+#Create a class that will find the numbers in the tuple that add up to a sum and return the position of elements. The value of the sum can be taken as input from the user. Tuple - (10,20,30,40,50,60,70)
+
+class pairelements:
+    def twosum(self,nums,target):
+        lookup={}
+
+        for i,num in enumerate(nums):
+            if target-num in lookup:
+                return(lookup[target-num],i)
+            lookup[num]==i
+
+value=int(input("Enter sum for which you want to make this search:"))
+print("index1=%d, index2=%d" % pairelements().twosum((10,20,30,40,50,60,70),value))
---
+++ python codingal/dogbreed.py
@@ -0,0 +1,2 @@
+class dog:
+    pass
