f444bc6e8a8817e70b21f02c21be9de7b9f45d90
yerraabhi <yerraabhiram131@gmail.com>
2026-04-22 17:58:39 +0530

    initial commit

---
+++ python codingal/abc.py
@@ -0,0 +1,47 @@
+from abc import ABC,abstractmethod
+
+class absclass(ABC):
+    def print(self,x):
+        print("Passed value:",x)
+    
+    @abstractmethod
+    def task(self):
+        print("We are in absclass")
+
+class testclass(absclass):
+    def task(self):
+        print("We are in testclass task")
+
+testobj=testclass()
+testobj.task
+testobj.print(100)
+
+#Create two classes for two different countries that consist of three methods to display the following information of respective country - capital, language and type of country. Then, use Polymorphism to create a common interface for both classes.
+
+class India:
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    
+    def language(self):
+        print("Hindi is the most widely spoken language of India.")
+    
+    def type(self):
+        print("India is a developing country.")
+
+class USA:
+    def capital(self):
+        print("Washington DC is the capital of USA.")
+    
+    def language(self):
+        print("English is the primary language of USA.")
+    
+    def type(self):
+        print("USA is a developed country.")
+
+objind=India()
+objusa=USA()
+
+for country in (objind,objusa):
+    country.capital()
+    country.language()
+    country.type()
--- python codingal/busfare.py
+++ python codingal/busfare.py
@@ -2,4 +2,13 @@ class Vehicle:
     def __init__(self,seating):
         self.seating=seating
 
-    def 
+
+    def fare(self):
+        return self.seating*110
+
+class Bus(Vehicle):
+    pass
+
+bus1=Bus(50)
+
+bus1.fare()
